da2091f4f8f6d8173c3202f091504a848ece5553
jjgaytanh <juan.jose.gaytan@tec.mx>
2022-09-19 16:42:54 -0500

    v1.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def module_path():
 dir_path = module_path()
 long_description_file = dir_path + "/README.md"
 setup(name='geometria',
-    version='1.2',
+    version='1.3',
     url=None,
     license=None,
     author='Juan Jose Gaytan Hernandez Magro',
